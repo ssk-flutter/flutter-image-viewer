73a33458152e1228b96f57eb8f055e73df2166f3
ssk <developer.kss@gmail.com>
2022-06-03 21:48:41 +0900

    left / right arrow

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -15,6 +15,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final ImageRepository _repository = ImageRepository();
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -30,15 +31,47 @@ class _HomePageState extends State<HomePage> {
                 width: constraints.maxWidth,
                 height: constraints.maxHeight,
                 child: Image.network(
-                  _repository.images.first.image,
+                  _repository.images[index].image,
                   fit: BoxFit.contain,
                 ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Row(
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          index -= 1;
+                          if (index < 0) {
+                            index = 0;
+                          }
+                        });
+
+                        print('index is $index');
+                      },
+                      icon: Icon(Icons.keyboard_arrow_left)),
+                  Spacer(),
+                  IconButton(
+                      onPressed: () {
+                        setState(() {
+                          index += 1;
+                          if (_repository.images.length <= index) {
+                            index = _repository.images.length - 1;
+                          }
+                        });
+
+                        print('index is $index');
+                      },
+                      icon: Icon(Icons.keyboard_arrow_right)),
+                ],
               ),
             ),
             Positioned(
               left: 18,
               bottom: 18,
-              child: TimestampWidget(timestamp: _repository.images.first.time),
+              child: TimestampWidget(timestamp: _repository.images[index].time),
             ),
           ],
         ));
